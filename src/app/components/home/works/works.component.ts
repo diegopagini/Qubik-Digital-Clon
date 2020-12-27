81c7868e6d002eecb4e5c8e3f78889d82df17cf8
Diego Pagini <diegopagini777@gmail.com>
2020-12-27 13:37:07 -0300

    carousel

--- src/app/components/home/works/works.component.ts
+++ src/app/components/home/works/works.component.ts
@@ -22,22 +22,18 @@ export class WorksComponent implements OnInit {
 
   ngOnInit(): void {
     let mySwiper = new Swiper('.swiper-container', {
-      // Optional parameters
+      slidesPerView: 4,
       direction: 'horizontal',
       loop: true,
-
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-
-      // Navigation arrows
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-
-      // And if we need scrollbar
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: false,
+      },
       scrollbar: {
         el: '.swiper-scrollbar',
       },
